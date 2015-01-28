2b0a43c47bf4066836abbabda4249c6303ec7758
Andres Yepes <yepes07@gmail.com>
2015-01-28 16:41:30 -0500

    refactor code into a more MVC look

---
+++ models/orders.js
@@ -0,0 +1,21 @@
+var db = require("../lib/mongodb");
+
+var _all = function(callback){
+  db.get("orders").find({}, callback);
+};
+
+var _insert = function(order, callback){
+  _remove(order, function(err, doc){
+    if(err) console.log("error removing from db: " + err);
+    db.get("orders").insert(order, callback);
+  });
+};
+
+var _remove = function(order, callback){
+  db.get("orders").remove({id: order.id}, callback);
+};
+
+
+exports.all    = _all;
+exports.insert = _insert;
+exports.remove = _remove;
